5385519e2206176227953a5b775cd01c8e4bcd7f
Iain <icleen@gmail.com>
2017-01-10 16:31:16 -0700

    fixed some file reading errors

--- src/dev/ImageEditor/IainLee/Image.java
+++ src/dev/ImageEditor/IainLee/Image.java
@@ -1,21 +1,58 @@
 package dev.ImageEditor.IainLee;
 
-import java.util.ArrayList;
-
 public class Image {
 	
-	private static ArrayList<Pixel> pixels;
-	private int width, length;
+	private static Pixel[][] pixels;
+	private int width, height;
+	private int col, row;
 	
-	public Image( int width, int length ) {
-		pixels = new ArrayList<>();
+	public Image( int width, int height ) {
+		pixels = new Pixel[height][width];
 		this.width = width;
-		this.length = length;
+		this.height = height;
+		col = 0;
+		row = 0;
 	}
 	
 	public void addPixel( int red, int green, int blue ) {
-		Pixel pix = new Pixel( red, green, blue );
-		pixels.add(pix);
+		try {
+			Pixel pix = new Pixel( red, green, blue );
+			pixels[row][col] = pix;
+			row++;
+			col++;
+			if( row >= height ) {
+				row = 0;
+			}if( col >= width ) {
+				col = 0;
+			}
+		} catch( ArrayIndexOutOfBoundsException e ) {
+			System.out.println( "the row or col was out of bounds for some reason" );
+			e.printStackTrace();
+		}
+	}
+	
+	public void grayscale() {
+		
+	}
+	
+	public void invert() {
+		
+	}
+	
+	public void emboss() {
+		
+	}
+	
+	public void motionblur( String degree ) {
+		int deg = Integer.parseInt( degree );
+		
+	}
+	
+	public int getWidth() {
+		return width;
 	}
 
+	public int getHeight() {
+		return height;
+	}
 }
--- src/dev/ImageEditor/IainLee/ImageEditor.java
+++ src/dev/ImageEditor/IainLee/ImageEditor.java
@@ -1,7 +1,13 @@
 package dev.ImageEditor.IainLee;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 
 public class ImageEditor {
@@ -13,37 +19,98 @@ public class ImageEditor {
 	private static Image image;
 	
 	public static void main( String[] args ) {
-		if( args.length < 3 || args.length > 5) {
+		if( args.length < 3 || args.length > 4 ) {
 			System.out.println( "Incorrect input format\n" + USAGE );
 			return;
 		}
 		try {
-			FileInputStream in = new FileInputStream( args[0] );
+			InputStream inStream = Files.newInputStream( Paths.get( args[0] ) );
+			Reader in = new InputStreamReader( inStream, StandardCharsets.US_ASCII );
 			
-			int content = in.read();
-			int width = in.read();
-			int length = in.read();
-			image = new Image( width, length );
-			
-			content = in.read();
-			int r, g, b;
-			while( content != -1 ) {
-				r = (int) content;
-				content = in.read();
-				g = (int) content;
-				content = in.read();
-				b = (int) content;
-				content = in.read();
-				image.addPixel( r, g, b );
+			List<String> lines = Files.readAllLines( Paths.get(args[0]), StandardCharsets.US_ASCII );
+			image = new Image( 1000, 750 );
+			int i;
+			for( i = 0; i < lines.size(); i++ ) {
+				System.out.println( lines.get(i) );
+				if( isNum( lines.get(i) ) ) {
+					break;
+				}
 			}
+			if( i >= lines.size() ) {
+				System.out.println( "this is an invalid file" );
+				return;
+			}
+			
 			
 			in.close();
 		} catch (IOException e) {
-			System.out.println( "file not found\n" + USAGE );
+			System.out.println( "file not found" );
 			e.printStackTrace();
 		}
 		
+		if( args[2].equals("grayscale") ) {
+			image.grayscale();
+		}else if( args[2].equals("invert") ) {
+			image.invert();
+		}else if( args[2].equals("emboss") ) {
+			image.emboss();
+		}else if( args[2].equals("motionblur") ) {
+			if( args[3] == null ) {
+				System.out.println( "Incorrect input format\n" + USAGE );
+				return;
+			}
+			image.motionblur( args[3] );
+		}else {
+			System.out.println( "Incorrect input format\n" + USAGE );
+			return;
+		}
 		
+//		try {	
+//			FileOutputStream out = new FileOutputStream( args[1] );
+//			
+//			out.write( Integer.parseInt("P3 ") );
+//			out.write( image.getWidth() );
+//			out.write( Integer.parseInt(" ") );
+//			out.write( image.getHeight() );
+//			out.write( Integer.parseInt(" ") );
+//			
+//			out.close();
+//		} catch (FileNotFoundException e) {
+//			System.out.println( "Cannot find or create the specified file!" );
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			System.out.println( "Could not write the file out" );
+//			e.printStackTrace();
+//		}
 		
 	}
+
+	private static boolean isNum( String s ) {
+		char c = s.charAt(0);
+		System.out.println( c );
+		switch( c ) {
+		case '0':
+			return true;
+		case '1':
+			return true;
+		case '2':
+			return true;
+		case '3':
+			return true;
+		case '4':
+			return true;
+		case '5':
+			return true;
+		case '6':
+			return true;
+		case '7':
+			return true;
+		case '8':
+			return true;
+		case '9':
+			return true;
+		}
+		return false;
+	}
+	
 }
