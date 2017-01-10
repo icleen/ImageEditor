958d80a5c6f6c799a09f80786561485b4f81411e
Iain <icleen@gmail.com>
2017-01-10 14:05:43 -0700

    formated the input reader to accomodate the information and make an image object

--- src/dev/ImageEditor/IainLee/Image.java
+++ src/dev/ImageEditor/IainLee/Image.java
@@ -1,11 +1,21 @@
 package dev.ImageEditor.IainLee;
 
+import java.util.ArrayList;
+
 public class Image {
 	
-	Pixel[] pixels;
+	private static ArrayList<Pixel> pixels;
+	private int width, length;
 	
-	public Image( int size ) {
-		pixels = new Pixel[size];
+	public Image( int width, int length ) {
+		pixels = new ArrayList<>();
+		this.width = width;
+		this.length = length;
+	}
+	
+	public void addPixel( int red, int green, int blue ) {
+		Pixel pix = new Pixel( red, green, blue );
+		pixels.add(pix);
 	}
 
 }
--- src/dev/ImageEditor/IainLee/ImageEditor.java
+++ src/dev/ImageEditor/IainLee/ImageEditor.java
@@ -6,20 +6,35 @@ import java.io.IOException;
 
 public class ImageEditor {
 	
+	enum Transform { grayscale, invert, emboss, motionblur };
+	
 	public final static String USAGE = "USAGE: java ImageEditor in-file out-file (grayscale|invert|emboss|motionblur motion-blur-length)";
 	
+	private static Image image;
+	
 	public static void main( String[] args ) {
-		if( args.length != 3) {
-			System.out.println( "Incorrect input format" );
+		if( args.length < 3 || args.length > 5) {
+			System.out.println( "Incorrect input format\n" + USAGE );
 			return;
 		}
 		try {
 			FileInputStream in = new FileInputStream( args[0] );
 			
 			int content = in.read();
+			int width = in.read();
+			int length = in.read();
+			image = new Image( width, length );
+			
+			content = in.read();
+			int r, g, b;
 			while( content != -1 ) {
-				System.out.print( (char) content );
+				r = (int) content;
 				content = in.read();
+				g = (int) content;
+				content = in.read();
+				b = (int) content;
+				content = in.read();
+				image.addPixel( r, g, b );
 			}
 			
 			in.close();
@@ -27,5 +42,8 @@ public class ImageEditor {
 			System.out.println( "file not found\n" + USAGE );
 			e.printStackTrace();
 		}
+		
+		
+		
 	}
 }
