dc3cd0f92f0c3ed53c1382cedcc194dcf0eaf8f4
Iain <icleen@gmail.com>
2017-01-12 14:25:26 -0700

    i got the emboss function to work now too;  just have to do the blur

--- src/dev/ImageEditor/IainLee/Image.java
+++ src/dev/ImageEditor/IainLee/Image.java
@@ -51,6 +51,62 @@ public class Image {
 	
 	public void emboss() {
 		System.out.println( "emboss" );
+		try {
+			int reddif, grndif, bludif;
+			for( int i = height - 1; i > 0; i-- ) {
+				for( int j = width - 1; j > 0; j-- ) {
+					reddif = pixels[i][j].getRed() - pixels[i - 1][j - 1].getRed();
+					grndif = pixels[i][j].getGreen() - pixels[i - 1][j - 1].getGreen();
+					bludif = pixels[i][j].getBlue() - pixels[i - 1][j - 1].getBlue();
+					pixels[i][j].setRed( maxDif( reddif, grndif, bludif ) );
+					pixels[i][j].setGreen( maxDif( reddif, grndif, bludif ) );
+					pixels[i][j].setBlue( maxDif( reddif, grndif, bludif ) );
+				}
+			}
+			for( int j = width - 1; j > 0; j-- ) {
+				pixels[0][j].setRed(128);
+				pixels[0][j].setGreen(128);
+				pixels[0][j].setBlue(128);
+			}
+			for( int j = height - 1; j >= 0; j-- ) {
+				pixels[j][0].setRed(128);
+				pixels[j][0].setGreen(128);
+				pixels[j][0].setBlue(128);
+			}
+			
+		} catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println( "it went out of bounds in emboss" );
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private int maxDif( int a, int b, int c ) {
+		int v = 0;
+		int a2 = a;
+		int b2 = b;
+		int c2 = c;
+		if( a < 0 ) {
+			a2 *= -1;
+		}if( b < 0 ) {
+			b2 *= -1;
+		}if( c < 0 ) {
+			c2 *= -1;
+		}
+		if( a2 > b2 && a2 > c2 ) {
+			v = a;
+		}else if( b2 > c2 && b2 > a2 ) {
+			v =  b;
+		}else {
+			v = c;
+		}
+		v += 128;
+		if( v < 0 ) {
+			v = 0;
+		}else if( v > 255 ) {
+			v = 255;
+		}
+		return v;
 	}
 	
 	public void motionblur( String degree ) {
